f8d594dfe77eadee1415701179a421ff72a2c9bf
Daniel De Lucca <delucca@pm.me>
2021-03-20 22:24:04 +0000

    wip

--- etl.py
+++ etl.py
@@ -1,21 +1,87 @@
 from lojaintegrada import Api
 from os import environ
 from sys import argv
+import pprint
+
+def build_lojaintegrada_api(api_key=environ['LOJA_INTEGRADA_API_KEY'], app_key=environ['LOJA_INTEGRADA_APP_KEY']):
+  return Api(api_key, app_key)
+
+lojaintegrada_api = build_lojaintegrada_api()
 
 def main():
-  lojaintegrada_api = build_lojaintegrada_api()
-  lojaintegrada_orders = get_lojaintegrada_orders(lojaintegrada_api)
-  print(lojaintegrada_orders)
+  extracted_data = extract_data()
+  transformed_data = transform_data(extracted_data)
 
-def build_lojaintegrada_api(apiKey=environ['LOJA_INTEGRADA_API_KEY'], appKey=environ['LOJA_INTEGRADA_APP_KEY']):
-  return Api(apiKey, appKey)
+  print(transformed_data)
 
-def get_lojaintegrada_orders(api):
+def extract_data():
+  lojaintegrada_orders = get_lojaintegrada_orders()
+
+  return lojaintegrada_orders
+
+def get_lojaintegrada_orders():
   from_date = argv[1]
-  order_pages = api.get_paid_orders(since_criado=from_date)
-  orders = [api.get_order(order['numero']) for order_page in order_pages for order in order_page['objects']]
+  order_pages = lojaintegrada_api.get_paid_orders(since_criado=from_date)
+  orders = [lojaintegrada_api.get_order(order['numero']) for order_page in order_pages for order in order_page['objects']]
 
   return orders
+
+def transform_data(raw_data):
+  transformed_data = [transform_single_order(order) for order in raw_data]
+
+  return transformed_data
+
+def transform_single_order(order):
+  transformed_order = {
+    'order_no': build_order_no(order),
+    'client_name': build_client_name(order),
+    'delivery_method': build_delivery_method(order),
+    'deadline': build_deadline(order),
+    'client_comment': build_client_comment(order),
+    'products': build_products(order),
+    'products_availability': build_products_availability(order)
+  }
+
+  return transformed_order
+
+def build_order_no(order):
+  return order['numero']
+
+def build_client_name(order):
+  return order['cliente']['nome']
+
+def build_delivery_method(order):
+  primary_method = order['envios'][0]
+  
+  return primary_method['forma_envio']['tipo']
+
+def build_deadline(order):
+  slowest_availability = min([item['disponibilidade'] for item in order['itens']])
+
+  return slowest_availability
+
+def build_client_comment(order):
+  return order['cliente_obs']
+
+def build_products(order):
+  products = [build_single_product(order_product) for order_product in order['itens']]
+
+  return ',\n'.join(products)
+
+def build_single_product(order_product):
+  id = order_product['produto'].split('/')[-1]
+  product = lojaintegrada_api.get_product(id)
+  quantity = int(float(order_product['quantidade']))
+
+  return '{} {}'.format(quantity, product['nome'])
+
+def build_products_availability(order):
+  products_availability = [build_single_product_availability(order_product) for order_product in order['itens']]
+
+  return ', '.join(products_availability)
+
+def build_single_product_availability(order_product):
+  return str(order_product['disponibilidade'])
 
 if __name__ == '__main__':
   main()
